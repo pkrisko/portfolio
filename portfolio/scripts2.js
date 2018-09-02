317a1b24d0b43a2e42003c6c5120e23748f4d621
Krisko <patrick.krisko@va1macwg099hv2t.int.asurion.com>
2018-09-02 11:41:33 -0400

    Setup

---
+++ portfolio/scripts2.js
@@ -0,0 +1,24 @@
+// function setup() {
+//     createCanvas(640, 480);
+//   }
+
+//   function draw() {
+//     if (mouseIsPressed) {
+//       fill(0);
+//     } else {
+//       fill(255);
+//     }
+//     ellipse(mouseX, mouseY, 80, 80);
+//   }
+
+// function setup() {
+//     createCanvas(600, 600, WEBGL);
+//   }
+
+//   function draw() {
+//     background(200);
+//     rotateX(frameCount * 0.01);
+//     rotateY(frameCount * 0.01);
+//     torus(400, 100);
+//   }
+
